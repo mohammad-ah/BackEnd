46db03c845aee7c74915b18e29053c02c912c008
Mohammad <mohamad.ma09@gmail.com>
2020-04-05 21:46:53 -0500

    fix user schema

--- models/user.js
+++ models/user.js
@@ -15,6 +15,16 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    followers: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }],
+    following: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }],
     createdat: {
         type: Date,
         default: Date.now
