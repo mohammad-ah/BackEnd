1f260d11a05887f310adef259ae8e7a9a259e7f5
Mohammad <mohamad.ma09@gmail.com>
2020-04-06 00:08:00 -0500

    add get following list feature

--- controller/user.js
+++ controller/user.js
@@ -8,3 +8,57 @@ exports.signUp = async (req, res, next) => {
         next(err);
     }
 };
+
+exports.followUser = async (req, res, next) => {
+    try {
+        const following = await User.findById(req.body.followingId);
+        const follower = await User.findById(req.body.followerId);
+        follower.following.push(req.body.followingId);
+        following.followers.push(req.body.followerId);
+
+        following.save();
+        follower.save();
+
+        res.status(200).send({ message: "User followed successfully.", user: follower});
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.unfollowUser = async (req, res, next) => {
+    try {
+        const following = await User.findById(req.body.followingId);
+        const follower = await User.findById(req.body.followerId);
+
+        const followerIndex = follower.following.findIndex(user => new String(user._id).trim() === new String(req.body.followingId).trim());
+        if (followerIndex >= 0) {
+            console.log("followerIndex", followerIndex);
+            follower.following.splice(followerIndex, 1);
+            console.log("follower.following", follower.following);
+        }
+        const followingIndex = following.followers.findIndex(user => new String(user._id).trim() === new String(req.body.followerId).trim());
+        if (followingIndex >= 0) {
+            console.log("followingIndex", followingIndex);
+            following.followers.splice(followingIndex, 1);
+            console.log("following.followers", following.followers)
+        }
+
+        following.save();
+        follower.save();
+
+        res.status(200).send({ message: "User unfollowed successfully.", user: follower});
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.followingList = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        console.log(user);
+        res.status(200).send({ message: "Following List get successfully.", following: user.following});
+    } catch (err) {
+        next(err);
+    }
+
+};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,10 @@ const userController = require('../controller/user');
 
 /* GET users listing. */
 router.post('/user-register', userController.signUp);
+router.post('/follow', userController.followUser);
+router.post('/unfollow', userController.unfollowUser);
+router.get('/following/:id', userController.followingList);
+// router.post('/disable-notifications', userController.disableNotifications);
+// router.post('/follow', userController.followUser);
 
 module.exports = router;
