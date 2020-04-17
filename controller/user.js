64ec5b102a05b10cc4aebbb0edad559bf78589b0
Mohammad <mohamad.ma09@gmail.com>
2020-04-17 18:07:18 -0500

    adding something!

--- controller/user.js
+++ controller/user.js
@@ -1,10 +1,25 @@
 const User = require('../models/user');
 const Activation = require('../models/activation');
 
+
 exports.signUp = async (req, res, next) => {
+    const password = req.body.password;
+    delete req.body.password;
+    console.log(req.body);
+    console.log(password);
+
     try {
-        await new User(req.body).save();
-        res.status(200).send({ message: "User created successfully."});
+        const user = await new User(req.body).save();
+        console.log(user);
+        user.setPassword(password);
+        user.save(function(err) {
+            let token;
+            token = user.generateJwt();
+            res.status(200).send({ message: "User created successfully.", user: user, "token" : token});
+
+        });
+        console.log(user);
+
     } catch (err) {
         next(err);
     }
@@ -12,12 +27,12 @@ exports.signUp = async (req, res, next) => {
 
 exports.followUser = async (req, res, next) => {
     try {
-        const following = await User.findById(req.body.followingId);
+        const user = await User.findById(req.body.id);
         const follower = await User.findById(req.body.followerId);
-        follower.following.push(req.body.followingId);
-        following.followers.push(req.body.followerId);
+        follower.followers.push(req.body.id);
+        user.following.push(req.body.followerId);
 
-        following.save();
+        user.save();
         follower.save();
 
         res.status(200).send({ message: "User followed successfully.", user: follower});
@@ -28,26 +43,29 @@ exports.followUser = async (req, res, next) => {
 
 exports.unfollowUser = async (req, res, next) => {
     try {
+        const userF = await User.findById(req.body.id);
         const following = await User.findById(req.body.followingId);
-        const follower = await User.findById(req.body.followerId);
 
-        const followerIndex = follower.following.findIndex(user => new String(user._id).trim() === new String(req.body.followingId).trim());
+        const followerIndex = userF.following.findIndex(user => new String(user._id).trim() === new String(req.body.followingId).trim());
         if (followerIndex >= 0) {
             console.log("followerIndex", followerIndex);
-            follower.following.splice(followerIndex, 1);
-            console.log("follower.following", follower.following);
+            userF.following.splice(followerIndex, 1);
+            console.log("follower.following", userF.following);
         }
-        const followingIndex = following.followers.findIndex(user => new String(user._id).trim() === new String(req.body.followerId).trim());
+        const followingIndex = following.followers.findIndex(user => new String(user._id).trim() === new String(req.body.id).trim());
         if (followingIndex >= 0) {
             console.log("followingIndex", followingIndex);
             following.followers.splice(followingIndex, 1);
             console.log("following.followers", following.followers)
         }
 
-        following.save();
-        follower.save();
+        console.log(userF)
+        console.log(following)
 
-        res.status(200).send({ message: "User unfollowed successfully.", user: follower});
+        userF.save();
+        following.save();
+
+        res.status(200).send({ message: "User unfollowed successfully.", user: userF});
     } catch (err) {
         next(err);
     }
@@ -100,7 +118,14 @@ exports.requestActivation = async(req, res, next) => {
 
 exports.listUsers = async(req, res, next) => {
     try {
-        const users = await User.find();
+        const user = await User.findOne({_id: req.body.id});
+        const users = await User.find(
+            {
+                "following": {
+                    "$nin": user.following
+                }
+            }
+        );
         res.status(200).send({message: "success.", users: users});
     } catch (err) {
         next(err);
@@ -114,6 +139,30 @@ exports.userFollowings = async(req, res, next) => {
         );
         const followings = user.following;
         res.status(200).send({message: "success.", followings: followings});
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.login = async (req, res, next) => {
+    console.log('start')
+    const password = req.body.password;
+    const email = req.body.username;
+    console.log(email)
+    try {
+        console.log('in 1')
+        const user = await User.findOne({email: email});
+        console.log(user)
+        if (user.validPassword(password))
+        {
+            console.log('in')
+            const token = user.generateJwt();
+            console.log(token)
+
+            res.status(200).send({ message: "User logged in successfully.", user: user, token: token});
+            return;
+        }
+        res.status(401).send({ message: "Password is incorrect."});
     } catch (err) {
         next(err);
     }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var postController = require('../controller/post')
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: 'MY_SECRET',
+    userProperty: 'payload'
+});
 
 /* GET home page. */
 router.get('/get-posts/:id&:skip&:limit', postController.getFollowingPosts);
