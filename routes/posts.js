57983a4b615ba6c5d5675b248a6e2b84d3250e41
Mohammad <mohamad.ma09@gmail.com>
2020-04-18 12:49:07 -0500

    fix multer issue

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var postController = require('../controller/post');
 
-const multer = require('multer');
-
-var DIR = '../public/';
-var upload = multer({dest: DIR}).single('image');
-
 router.post('/create-post', postController.createPost);
 router.post('/remove', postController.removePost);
 router.get('/search/:id&:search&:skip&:limit', postController.searchPosts);
