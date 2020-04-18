13ed10adf05d44674bbf3f5baee324a30c6a6946
Mohammad <mohamad.ma09@gmail.com>
2020-04-18 12:30:23 -0500

    fix public folder issue

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules', 'public')));
 
 /**
  * Routing
